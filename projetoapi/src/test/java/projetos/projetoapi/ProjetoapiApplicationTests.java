ccf0980bf126c3fb91f09bab46202d6b7bdd72d2
Diego Claudino <claudinodiego98@gmail.com>
2026-04-09 14:53:04 -0300

    Add files via upload

---
+++ projetoapi/src/test/java/projetos/projetoapi/ProjetoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package projetos.projetoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
